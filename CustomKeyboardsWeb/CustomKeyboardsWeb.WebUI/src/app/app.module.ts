0d5216174b5d2a6669fb107e13feaa75a2d4dfb7
Mateus Cambraia <mateus.cambraia@pris.com.br>
2023-03-30 02:01:11 -0300

    iniciando criação de componentes genericos e iniciando projeto de angular

--- CustomKeyboardsWeb/CustomKeyboardsWeb.WebUI/src/app/app.module.ts
+++ CustomKeyboardsWeb/CustomKeyboardsWeb.WebUI/src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { TemplateComponent } from './components/template/template.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavbarComponent } from './components/template/navbar/navbar.component';
+import { SidebarComponent } from './components/template/sidebar/sidebar.component';
+import { FormComponent } from './components/generics/form/form.component';
+import { ButtonComponent } from './components/generics/button/button.component';
+import { CardComponent } from './components/generics/card/card.component';
+import { TableComponent } from './components/generics/table/table.component';
+import { InputComponent } from './components/generics/form/input/input.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FormComponent,
+    ButtonComponent,
+    CardComponent,
+    TableComponent,
+    InputComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
